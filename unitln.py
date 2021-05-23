536a0f8070344b3e88722f844bb0bcc7f3cd444b
tech-keny <chanken7@icloud.com>
2021-05-23 12:57:10 +0900

    nまでの和

---
+++ unitln.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+input_line = int(input())
+
+x = 0
+for i in range(input_line+1):
+    x += i
+    
+print(x)javascript:void(0)
+    
---
+++ vovabulary_test.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+# 自分の得意な言語で
+# Let's チャレンジ！！
+a = int(input())
+
+result = 0
+count = 0
+for i in range(a):
+    n = input()
+    x = n.split()
+    print(x)
+    if x[0] == x[1]:
+            result +=2
+    else:      
+			# KEY関数とmax関数でrange取る！！↓↓
+        for j in range():
+            if len(x[0]) == len(x[1]) and x[0][j] != x[1][j]:
+                count +=1
+               
+            else:
+                result += 0
+                
+        if count == 1:
+            result +=1
+        else:
+            result +=0
+            
+
+print(result)
+            
+            
+            
+            
+    
