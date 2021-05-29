b251ae9501db8fde2e05309f4563ca0cfe7c8c30
tech-keny <chanken7@icloud.com>
2021-05-29 14:56:15 +0900

    ファックス用紙の回収

--- fax.py
+++ fax.py
@@ -1,27 +1,47 @@
-# coding: utf-8
-# 自分の得意な言語で
-# Let's チャレンジ！！
-def get1():
-    global count
-    for j in range(fax):
-        while int(t[j][0]) < int(t[j+1][0])+1:
-            count += int(li[2])
+m = int(input())
+paper = int(input())
 
-        elif int(t[j][0]) == int(t[j+1][0])+1 :
-            
-
-    
-
-    
-    
-maximum = int(input())
-fax = int(input())
 count = 0
-t = []
-for i in range(fax):
-    x = input()
-    li = x.split()
-    t.append(li)
-get1()
+mid_num = 0
+result = 0
 
+li = []
+
+
+
+for i in range(paper):
+    x = input().split()
+    x = list(map(int, x))
+    li.append(x)
+    time = li[0][0]
+    
+for j in range(paper):  
+    if time == li[j][0]:
+        if li[j][1] <= 59:
+            count += li[j][2]
+    else:
+        if count % m ==0:
+            mid_num = count//m
+        else:
+            mid_num = count//m +1 
+            
+        result += mid_num
+        count = 0
+        if li[j][1] <= 59:
+            count += li[j][2]
+        mid_num = 0
+        time = li[j][0]
+        
+if count % m == 0:
+    mid_num += count//m
+else:
+    mid_num += count//m +1
+
+result += mid_num
+
+print(result)
+
+
+    
+    
     
