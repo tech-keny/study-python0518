c519e55a157894e2408245ab7a1896fd29b97468
tech-keny <chanken7@icloud.com>
2021-05-31 00:17:35 +0900

    C043使用回数の調査（１００点）

--- C043.py
+++ C043.py
@@ -4,13 +4,20 @@ x = input().split()
 
 new = list(map(lambda a:x.count(a),x))
 
+
 dl = {}
 
 for i in range(num):
     dl[x[i]] = new[i]
-    
+
 
 max_val = max(dl.values())
 keys_of_max_val = [key for key in dl if dl[key] == max_val]
 
-print(' '.join(keys_of_max_val))
+keys_of_max_val = list(map(int,keys_of_max_val))
+
+final_result = sorted(keys_of_max_val,reverse=False)
+
+final_result = list(map(str, final_result))
+
+print(' '.join(final_result))
